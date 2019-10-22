ff2a1d842bd47e99c7a5e2e26bbe550ffb8bf5e4
Thiago Butignon <thiagobutignon@gmail.com>
2019-10-21 22:29:24 -0300

    Adicionado um botao, target e perform segue programaticamente para uma navigation view

--- ProgramaticUI/FirstScreenViewController.swift
+++ ProgramaticUI/FirstScreenViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class FirstScreenViewController: UIViewController {
+    
+    let nextButton: UIButton = UIButton()
+    let nextLabel: UILabel = UILabel()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        self.view.backgroundColor = .red
+        self.setupNextButton()
     }
     
+    func setupNextButton() {
+        self.nextButton.backgroundColor = .white
+        self.nextButton.setTitleColor(.red, for: .normal)
+        self.nextButton.setTitle("Next", for: .normal)
+        
+        self.nextButton.addTarget(self, action: #selector(self.nextButtonTapped), for: .touchUpInside)
+        
+        self.view.addSubview(nextButton)
+        self.setNextButtonContraints()
+    }
+    
+    @objc func nextButtonTapped() {
+        let nextScreen: SecondScreenViewController = SecondScreenViewController()
+        nextScreen.title = "Oh Yeah!"
+        navigationController?.pushViewController(nextScreen, animated: true)
+    }
 
+    func setNextButtonContraints() {
+        self.nextButton.translatesAutoresizingMaskIntoConstraints = false
+        self.nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        self.nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        self.nextButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        self.nextButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
 
 }
