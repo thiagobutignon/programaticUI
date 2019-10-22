8a193fb1566bd794b1d5034210faaf696c66d6c3
Thiago Butignon <thiagobutignon@gmail.com>
2019-10-21 21:32:30 -0300

    First commit

---
+++ ProgramaticUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgramaticUI
+//
+//  Created by Thiago B Claramunt on 21/10/19.
+//  Copyright © 2019 Thiago B Claramunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
